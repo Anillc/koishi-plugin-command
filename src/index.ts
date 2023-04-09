08ca8d0fcc3bb562dc86e382c3d6657ffaf2300c
Anillc <i@anillc.cn>
2023-04-09 11:08:46 +0800

    support quote

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Grammar, accept, s, Productor } from 'iberis'
+import { Grammar, accept, s, Productor, ParsingError, Input } from 'iberis'
 import { Argv, Context, Logger, segment } from 'koishi'
 
 declare module 'koishi' {
@@ -24,16 +24,33 @@ export function apply(ctx: Context) {
     const content = parsed.prefix
       ? parsed.content.slice(parsed.prefix.length)
       : parsed.content
-    const nodes = grammar.parse(s.lexer(content), s.equals)
-    if (nodes.length !== 1) return
-    session.argv = { args: [], options: {}, session }
-    accept(nodes[0], session.argv)
+    try {
+      const nodes = grammar.parse(s.lexer(content), s.equals)
+      if (nodes.length !== 1) return
+      session.argv = { args: [], options: {}, session }
+      accept(nodes[0], session.argv)
+    } catch (e) {
+      if (!(e instanceof ParsingError<string | RegExp>)) {
+        throw e
+      }
+    }
   }, true)
 }
 
 function update(ctx: Context): Grammar<string | RegExp, Argv> {
   const cmds = ctx.root.$commander._commands._commandList
   const g = new Grammar<string | RegExp, Argv>('root')
+  const strip = (replace: string) => {
+    return ({ text }: Input) => {
+      return text
+        .substring(1, text.length - 1)
+        .replace(new RegExp(`\\\\${replace}`, 'g'), replace)
+    }
+  }
+  g.p('quote').t(/"(?:[^"\\]|\\.)*"/).bind(strip('"'))
+  g.p('quote').t(/'(?:[^'\\]|\\.)*'/).bind(strip("'"))
+  g.p('quote').t(/“(?:[^”\\]|\\.)*”/).bind(strip('”'))
+  g.p('quote').t(/‘(?:[^’\\]|\\.)*’/).bind(strip('’'))
   for (const cmd of cmds) {
     const { name } = cmd
     g.p('root').n(name)
@@ -57,7 +74,7 @@ function update(ctx: Context): Grammar<string | RegExp, Argv> {
       } else {
         g.p(`${name}_args_${i}`).n(`${name}_args_${i}_factor`)
       }
-      addTypeTerm(g.p(`${name}_args_${i}_factor`), argument.type, (value, argv) => {
+      addTypeTerm(g, g.p(`${name}_args_${i}_factor`), argument.type, (value, argv) => {
         argv.args.push(value)
       })
     }
@@ -73,7 +90,7 @@ function update(ctx: Context): Grammar<string | RegExp, Argv> {
           g.p(`${name}_option`).t(/-|(--)/).t(optionName)
             .bind((_1, _2, argv) => argv.options[decl.name] = true)
         }
-        addTypeTerm(g.p(`${name}_option`).t(/-|(--)/).t(optionName), decl.type, (value, argv) => {
+        addTypeTerm(g, g.p(`${name}_option`).t(/-|(--)/).t(optionName), decl.type, (value, argv) => {
           argv.options[decl.name] = value
         })
       }
@@ -88,7 +105,7 @@ function update(ctx: Context): Grammar<string | RegExp, Argv> {
           g.p(`${name}_option`).t(symbol)
             .bind((_, argv) => argv.options[decl.name] = true)
         }
-        addTypeTerm(g.p(`${name}_option`).t(symbol), decl.type, (value, argv) => {
+        addTypeTerm(g, g.p(`${name}_option`).t(symbol), decl.type, (value, argv) => {
           argv[decl.name] = value
         })
       }
@@ -99,61 +116,79 @@ function update(ctx: Context): Grammar<string | RegExp, Argv> {
 }
 
 function addTypeTerm<P extends unknown[]>(
+  g: Grammar<string | RegExp, Argv>,
   productor: Productor<string | RegExp, Argv, P>,
   type: Argv.Type,
   accept: (value: any, argv: Argv) => void,
 ) {
-  const resolver = (resolve: (text: string, argv: Argv, args: any[]) => any) => {
-    return (...args: any[]) => {
-      const { text } = args.at(-2)
-      const argv = args.at(-1)
-      accept(resolve(text, argv, args), argv)
-    }
-  }
+  const name = `${productor.name}_type`
+  productor.n(name)
   if (!type || type === 'string') {
-    productor.t(/\S+/).bind(resolver((text) => segment.escape(text)))
+    g.p(name).n('quote').bind(accept)
+    g.p(name).t(/(?!['"“])\S+/).bind(({ text }, argv) => {
+      accept(segment.escape(text), argv)
+    })
   } else if (typeof type === 'function') {
-    productor.t(/\S+/).bind(resolver((text, { session }) => type(text, session)))
+    g.p(name).t(/(?!['"“])\S+/).bind(({ text }, argv) => {
+      accept(type(text, argv.session), argv)
+    })
+    g.p(name).n('quote').bind((text, argv) => {
+      accept(type(text, argv.session), argv)
+    })
   } else if (type === 'text') {
-    productor.t(/.+/).bind(resolver((text) => segment.escape(text)))
+    g.p(name).n('quote').bind(accept)
+    g.p(name).t(/(?!['"“]).+/).bind(({ text }, argv) => {
+      accept(segment.escape(text), argv)
+    })
   } else if (type === 'rawtext') {
-    productor.t(/.+/).bind(resolver((text) => text))
+    g.p(name).n('quote').bind(accept)
+    g.p(name).t(/(?!['"“]).+/).bind(({ text }, argv) => accept(text, argv))
   } else if (type === 'number') {
     // negative is conflict with option
     // set it as a standalone term
-    productor.t(/[+-]?/).t(/\d+(\.\d+)?/).bind(resolver((text, _, args) => {
-      return args.at(-3).text === '-' ? -text : +text
-    }))
+    g.p(name).t(/[+-]/).t(/\d+(\.\d+)?/).bind(({ text: operator }, { text: number }, argv) => {
+      accept(operator === '-' ? -number : +number, argv)
+    })
+    g.p(name).t(/\d+(\.\d+)?/).bind(({ text }, argv) => {
+      accept(+text, argv)
+    })
   } else if (type === 'natural' || type === 'integer') {
-    productor.t(/[+-]?/).t(/\d+/).bind(resolver((text, _, args) => {
-      return args.at(-3).text === '-' ? -text : +text
-    }))
+    g.p(name).t(/[+-]/).t(/\d+/).bind(({ text: operator }, { text: number }, argv) => {
+      accept(operator === '-' ? -number : +number, argv)
+    })
+    g.p(name).t(/\d+/).bind(({ text }, argv) => {
+      accept(+text, argv)
+    })
   } else if (type === 'posint') {
-    productor.t(/\+?\d+/).bind(resolver((text) => +text))
+    g.p(name).t(/\+?\d+/).bind(({ text }, argv) => {
+      accept(+text, argv)
+    })
   } else if (type === 'user') {
-    productor.t(/@\S+/).bind(resolver((text, { session }) => {
+    g.p(name).t(/@\S+/).bind(({ text }, argv) => {
       const segments = text.substring(1).split(':')
       if (segments.length >= 2) {
-        return segments.join(':')
+        accept(segments.join(':'), argv)
       } else if (segments.length !== 0) {
-        return `${session.platform}:${segments[0]}`
+        accept(`${argv.session.platform}:${segments[0]}`, argv)
       } else {
         throw new Error('invalid user type')
       }
-    }))
+    })
   } else if (type === 'channel') {
-    productor.t(/#\S+/).bind(resolver((text, { session }) => {
+    g.p(name).t(/#\S+/).bind(({ text }, argv) => {
       const segments = text.substring(1).split(':')
       if (segments.length >= 2) {
-        return segments.join(':')
+        accept(segments.join(':'), argv)
       } else if (segments.length !== 0) {
-        return `${session.platform}:${segments[0]}`
+        accept(`${argv.session.platform}:${segments[0]}`, argv)
       } else {
         throw new Error('invalid channel type')
       }
-    }))
+    })
   } else if (type instanceof RegExp) {
-    productor.t(type).bind(resolver((text) => text))
+    g.p(name).t(type).bind(({ text }, argv) => {
+      accept(text, argv)
+    })
   } else {
     logger.warn(`${type}: currently not supported`)
   }
